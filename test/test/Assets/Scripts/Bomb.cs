153f42a9f0816a7658bcced926d269a5292a425d
HibikiAraki <moon707.piano@icloud.com>
2021-06-04 17:57:13 +0900

    stage

---
+++ test/test/Assets/Scripts/Bomb.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject expIosionPrefab;  // 爆発エフェクトのプレハブ
+    public LayerMask levelMask; // ステージのレイヤー
+    private bool exploded = false;  // すでに爆発している場合　true
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 3秒後にExplode関数を実行
+        Invoke("Explode", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 爆弾が爆発するときの処理
+    private void Explode()
+    {
+        // 爆発の位置に爆発エフェクトを作成
+        Instantiate(expIosionPrefab, transform.position, Quaternion.identity);
+
+        // 爆弾を非表示にする
+        GetComponent < MeshRenderer>().enabled = false;
+
+        // 爆発した
+        exploded = true;
+        // 爆風を広げる
+        StartCoroutine(CreateExplosions(Vector3.forward)); // 上に広げる
+        StartCoroutine(CreateExplosions(Vector3.right)); // 右に広げる
+        StartCoroutine(CreateExplosions(Vector3.back)); // 下に広げる
+        StartCoroutine(CreateExplosions(Vector3.left)); // 左に広げる
+
+        transform.Find("Collider").gameObject.SetActive(false);
+
+        // 0.3秒後に非表示のした爆弾を削除
+        Destroy(gameObject, 0.3f);
+    }
+
+    // 爆風を広げる
+    private IEnumerator CreateExplosions(Vector3 direction)
+    {
+        // 2マス分ループする
+        for (int i = 1; i < 3;i++)
+        {
+            // ブロックとの当たり判定の結果を格納する変数
+            RaycastHit hit;
+
+            // 爆風を広げた先に何か存在するか確認
+            Physics.Raycast
+                (
+                transform.position + new Vector3(0, 0.5f, 0),
+                direction,
+                out hit,
+                1,
+                levelMask
+            );
+
+            // 暴風を広げた先に何も存在しない場合
+            if (!hit.collider)
+            {
+                // 爆風を広げるために、
+                // 爆発エフェクトのオブジェクトを作成
+                Instantiate
+                    (
+                    expIosionPrefab,
+                    transform.position + (i * direction),
+                    expIosionPrefab.transform.rotation
+                    );
+            }
+
+            // 爆風を広げた先にブロックが存在する場合
+            else
+            {
+                // 爆風はこれ以上広げない
+                break;
+            }
+
+            // 0.05秒待ってから、次のマスに爆風を広げる
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    // 他のオブジェクトがこの爆弾に当たったら呼び出される
+    public void OnTrigger(Collider other)
+    {
+        // まだ爆発していない
+        // かつ、この爆弾にぶつかったオブジェクトが爆発エフェクトの場合
+        if (!exploded && other.CompareTag("Explosion"))
+        {
+            // 2重に爆発処理が実行されないように
+            // すでに爆発処理が実行されている場合は止める
+            CancelInvoke("Explode");
+
+            // 爆発する
+            Explode();
+        }
+    }
+}
---
+++ test/test/Assets/Scripts/GlobalStateManager.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GlobalStateManager : MonoBehaviour
+{
+    private int deadPlayers = 0;    // 死亡したプレイヤーの数
+    private int deadPlayerNumber = -1;  // 死亡したプレイヤーの番号
+
+    public void PlayerDied (int playerNumber)
+    {
+        // 死亡したプレイヤーの数を増やす
+        deadPlayers++;
+
+        // 1人のプレイヤーが死亡したら
+        if (deadPlayers == 1)
+        {
+            // 死亡したプレイヤーの番号を保持し
+            deadPlayerNumber = playerNumber;
+
+            // 0.3f秒後にCheckPlayersDeath関数を呼び出す
+            Invoke("CheckPlayersDeath", 0.3f);
+        }
+    }
+
+    void CheckPlayersDeath()
+    {
+        // 死亡したプレイヤーが1人だけの場合
+        if (deadPlayers == 1)
+        {
+            // プレイヤー1が死亡した場合
+            if (deadPlayerNumber == 1)
+            {
+                // プレイヤー2が勝利した
+                Debug.Log("プレイヤー2の勝利!!");
+            }
+            // プレイヤー2が死亡した場合
+            else
+            {
+                // プレイヤー1が勝利した
+                Debug.Log("プレイヤー1の勝利!!");
+            }
+        }
+        // すべてのプレイヤーが死亡した場合
+        else
+        {
+            // 引き分け
+            Debug.Log("引き分け");
+        }
+    }
+}
